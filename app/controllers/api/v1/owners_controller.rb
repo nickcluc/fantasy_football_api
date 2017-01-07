716139f9fca4de42fd3406c3c0c1d0dbd0d329f7
Nicholas Clucas <nclucas01@gmail.com>
2017-01-07 15:57:59 -0500

    allow callbacks in controller

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::OwnersController < Api::V1::BaseController
   def index
-    render json: Owner.includes(:teams).sorted_by_average_regular_season_score
+    render json: Owner.includes(:teams).sorted_by_average_regular_season_score, callback: params['callback']
   end
 end
