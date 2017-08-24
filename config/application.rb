952359b27965eb89e5069ccef31e2841ca2a6d10
Nick Clucas <nick.clucas@drugdev.com>
2017-08-24 14:09:15 -0400

    cors move

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module SabermetricsGuy
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins ENV['FRONTEND_URL']
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :patch, :options, :head]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
