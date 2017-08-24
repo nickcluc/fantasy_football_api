7a9b076829a8ddd09433dcd30e2fe80866c7371f
Nick Clucas <nick.clucas@drugdev.com>
2017-08-24 13:55:13 -0400

    cors move

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,13 @@ Bundler.require(*Rails.groups)
 
 module SabermetricsGuy
   class Application < Rails::Application
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ENV['FORNTEND_URL']
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
