efe7b43291e7f4b2d77f5dd8b24ad76ff876ef3f
Nicholas Clucas <nclucas01@gmail.com>
2018-04-05 00:00:19 -0400

    cleanup

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,6 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
+
   field :owners, !types[Types::OwnerType] do
     resolve -> (obj, args, ctx) { Owner.all }
   end
