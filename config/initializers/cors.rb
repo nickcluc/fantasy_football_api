ff9897cc3d99d7d647a7d4c34b7a714ea8c630ce
Nick Clucas <nclucas01@gmail.com>
2018-07-26 22:08:42 -0400

    Deployment (#3)
    * upgrade ruby version
    
    * upgrade rails/dependencies
    
    * modify framework inits
    
    * fix nil in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONTEND_URL']
+    origins ENV['FRONTEND_URL'] || ''
 
     resource '*',
       headers: :any,
