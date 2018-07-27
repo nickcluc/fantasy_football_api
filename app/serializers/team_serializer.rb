5b272a8b248a1a40408e1f4fb306d6dd3acc415c
Nick Clucas <nclucas01@gmail.com>
2018-07-27 01:49:24 -0400

    Develop (#4)
    * upgrade ruby version
    
    * upgrade rails/dependencies
    
    * modify framework inits
    
    * fix nil in cors
    
    * start using JSON API

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,9 +1,12 @@
-class TeamSerializer < ActiveModel::Serializer
+class TeamSerializer
+  include FastJsonapi::ObjectSerializer
   attributes :id,
-  :league_year,
-  :name,
-  :points_for,
-  :points_against,
-  :wins,
-  :losses
+    :league_year,
+    :name,
+    :points_for,
+    :points_against,
+    :wins,
+    :losses
+
+  set_key_transform :dash
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :owners, only: [:index]
+      resources :teams, only: [:show]
     end
   end
 end
