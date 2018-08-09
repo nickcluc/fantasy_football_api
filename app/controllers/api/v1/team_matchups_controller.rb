ae811099be4f88254d75de7caa680b4c28b37e33
Nicholas Clucas <nclucas01@gmail.com>
2018-08-09 09:41:43 -0400

    revert reg season changes

--- app/controllers/api/v1/team_matchups_controller.rb
+++ app/controllers/api/v1/team_matchups_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TeamMatchupsController < Api::V1::BaseController
   def index
     if params[:filter]
-      @team_matchups = TeamMatchup.find(params[:filter][:id].split(',')).keep_if{|mu| mu.regular_season == true}
+      @team_matchups = TeamMatchup.find(params[:filter][:id].split(','))
       if params[:filter][:regular_season]
         @team_matchups.keep_if{|mu| mu.regular_season == true}
       end
