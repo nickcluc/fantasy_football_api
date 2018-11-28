ccac65539594007a0fce6f093221ba79b450405c
Nicholas Clucas <nclucas01@gmail.com>
2018-11-28 09:54:59 -0500

    ordering

--- app/controllers/api/v1/team_matchups_controller.rb
+++ app/controllers/api/v1/team_matchups_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::TeamMatchupsController < Api::V1::BaseController
   def index
     if params[:filter]
-      @team_matchups = TeamMatchup.find(params[:filter][:id].split(','))
+      @team_matchups = TeamMatchup.find(params[:filter][:id].split(',')).order(season_id: :asc, week_number: :asc)
       if params[:filter][:regular_season]
         @team_matchups.keep_if{|mu| mu.regular_season == true}
       end
     else
-      @team_matchups = TeamMatchup.all
+      @team_matchups = TeamMatchup.order(season_id: :asc, week_number: :asc).all
     end
-    @team_matchups = @team_matchups.order(season_id: :asc, week_number: :asc)
+
     render json: TeamMatchupSerializer.new(@team_matchups).serialized_json, callback: params['callback']
   end
 end
